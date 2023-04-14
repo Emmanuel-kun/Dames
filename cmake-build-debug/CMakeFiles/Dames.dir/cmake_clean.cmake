46c3367efe4d8a6bf72a376419fe6921d030a7b3
Franki444 <frguillaume@orange.fr>
2023-04-14 11:11:49 +0200

    commit franck

---
+++ cmake-build-debug/CMakeFiles/Dames.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dames.dir/dames.c.obj"
+  "CMakeFiles/Dames.dir/dames.c.obj.d"
+  "CMakeFiles/Dames.dir/main.c.obj"
+  "CMakeFiles/Dames.dir/main.c.obj.d"
+  "Dames.exe"
+  "Dames.exe.manifest"
+  "Dames.pdb"
+  "libDames.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Dames.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Dames.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Dames.
+# This may be replaced when dependencies are built.
